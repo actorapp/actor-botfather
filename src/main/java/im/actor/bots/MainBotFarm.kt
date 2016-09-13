ce42f734b3aa82de5076b4f099b0b99fbe5a81e1
Steve Kite <steve@actor.im>
2016-09-13 22:16:52 +0300

    fix(botfather): Fixing endpoints

--- src/main/java/im/actor/bots/MainBotFarm.kt
+++ src/main/java/im/actor/bots/MainBotFarm.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     var bot_username = System.getenv("BOT_FATHER_USERNAME")
     var bot_token = System.getenv("BOT_FATHER_TOKEN")
     var bot_trace = System.getenv("BOT_FATHER_TRACE")
-    
+
     if (bot_username == null) {
         throw RuntimeException("Please, set BOT_FATHER_USERNAME environment variable")
     }
@@ -18,7 +18,7 @@ fun main(args: Array<String>) {
 
     farm("NewFarm") {
         if (bot_endpoint != null) {
-            endpoint = "wss://api.actor.im"
+            endpoint = bot_endpoint
         }
         bot(BotFatherBot::class) {
             name = bot_username
